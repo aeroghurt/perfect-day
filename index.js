6c0c1ba2f961bf909ad4c9d0883340ef887ce7a7
aeroplaneehheh <unluckyluckycloverrr@gmail.com>
2026-08-29 19:05:47 +0800

    functionality

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 // idk man and then pull a random song??
 
 
-async function getRandomGenre() {
+async function getRandomTrack() {
     let randomNumber = Math.floor(Math.random() * 50);
     const response = await axios.get(`https://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key=1ab15445a60aeb6ac38fa4a58d776a53&format=json`);
-    const genre = response.data.toptags.tag[randomNumber].name
-    console.log(genre)
+    const genre = response.data.toptags.tag[randomNumber].name;
+    let randomNumber2 = Math.floor(Math.random() * 1000);
+    const response2 = await axios.get(`https://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${genre}&api_key=1ab15445a60aeb6ac38fa4a58d776a53&limit=1000&format=json`);
+    const trackName = `${response2.data.tracks.track[randomNumber2].name}`
+    const trackArtist = `${response2.data.tracks.track[randomNumber2].artist.name}`
+    document.querySelector('#content>h2').textContent = `${trackName} by ${trackArtist}`
 }
 
-async function getRandomTrack(genre) {
-    let randomNumber = Math.floor(Math.random() * 1000);
-    const response = await axios.get(`https://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${genre}&api_key=1ab15445a60aeb6ac38fa4a58d776a53&limit=1000&format=json`);
-    const trackName = response.data.tracks.track[randomNumber].name
-    const trackArtist = response.data.tracks.track[randomNumber].artist.name
-}
-getRandomGenre();
+document.querySelector('button').addEventListener('click', () => {
+    getRandomTrack();
+})
